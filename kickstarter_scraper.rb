eba3e8d8d0504d223641768852403a262d4a824d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-03 20:37:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,6 @@
 # require libraries/modules here
 require "nokogiri"
-require_relative '.fixtures/kickstarter.html'
+require_relative './fixtures/kickstarter.html'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
