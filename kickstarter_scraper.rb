26603c854c1bc7ef07d2fe6b1ea44b97e9f97e0d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-03 20:28:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,6 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
-  
+  project_doc = kickstarter.css("li.project.grid_4").first
+   
 end
